aadaab9da8812b5032143621c0c73b99701a6f41
Ben Lubar <ben.lubar@gmail.com>
2015-09-27 14:14:23 -0500

    users and categories, still need topics, posts, messages, and votes.

---
+++ index.js
@@ -0,0 +1,65 @@
+var async = require('async');
+var pg = require('pg');
+
+(function(Exporter) {
+	var _table_prefix;
+	var _url;
+	Exporter.setup = function(config, callback) {
+		_table_prefix = config.tablePrefix;
+		_url = "postgres://" + encodeURIComponent(config.dbuser) + ":" + encodeURIComponent(config.dbpass) + "@" + encodeURIComponent(config.dbhost) + ":" + config.dbport + "/" + encodeURIComponent(config.dbname);
+		callback(null, config);
+	};
+
+	Exporter.getPaginatedUsers = function(start, limit, callback) {
+		pg.connect(_url, function(err, client, done) {
+			if (err) {
+				return callback(err);
+			}
+
+			client.query('SELECT u.id AS _uid, u.email AS _email, u.username AS _username, u.created_at AS _joindate, u.name AS _fullname, u.blocked::int AS _banned, p.website AS _website, p.location AS _location, u.views AS _profileviews, CASE WHEN u.admin THEN \'administrator\' WHEN u.moderator THEN \'moderator\' ELSE \'\' END AS _level, s.likes_received AS _reputation, \'/users/\' || u.username_lower AS _path FROM ' + _table_prefix + 'users AS u LEFT JOIN ' + _table_prefix + 'user_profiles AS p ON u.id = p.user_id LEFT JOIN ' + _table_prefix + 'user_stats AS s ON u.id = s.user_id ORDER BY _uid ASC LIMIT $1::int OFFSET $2::int', [limit, start], function(err, result) {
+				done(err);
+
+				if (err) {
+					return callback(err);
+				}
+
+				var users = {};
+
+				result.rows.forEach(function(row) {
+					row._joindate = +row._joindate;
+					users[row._uid] = row;
+				});
+
+				callback(null, users);
+			});
+		});
+	};
+
+	Exporter.getPaginatedCategories = function(start, limit, callback) {
+		pg.connect(_url, function(err, client, done) {
+			if (err) {
+				return callback(err);
+			}
+
+			client.query('SELECT c.id AS _cid, c.name AS _name, c.description AS _description, c."position" AS _order, c.slug AS _slug, c.parent_category_id AS _parentCid, \'/c/\' || CASE WHEN c.parent_category_id IS NULL THEN \'\' ELSE (SELECT p.slug FROM ' + _table_prefix + 'categories AS p WHERE p.id = c.parent_category_id) || \'/\' END || c.slug AS _path FROM ' + _table_prefix + 'categories AS c ORDER BY _cid ASC LIMIT $1::int OFFSET $2::int', [limit, start], function(err, result) {
+				done(err);
+
+				if (err) {
+					return callback(err);
+				}
+
+				var categories = {};
+
+				result.rows.forEach(function(row) {
+					categories[row._cid] = row;
+				});
+
+				callback(null, categories);
+			});
+		});
+	};
+
+	Exporter.teardown = function(callback) {
+		callback();
+	};
+})(module.exports);
---
+++ test.js
@@ -0,0 +1,80 @@
+var fs = require('fs-extra');
+var async = require('async');
+var Exporter = require('./index');
+
+function getAll(name, callback) {
+	var start = 0;
+	var limit = 500000;
+	var done = false;
+	var result = {};
+	var fn = Exporter['getPaginated' + name];
+	if (!fn) {
+		throw 'Missing ' + name;
+	}
+
+	async.whilst(
+		function(err) {
+			if (err) {
+				return true;
+			}
+			return !done;
+		},
+		function(next) {
+			fn(start, limit, function(err, map) {
+				if (err) {
+					return next(err);
+				}
+				done = true;
+				for (var i in map) {
+					if (map.hasOwnProperty(i)) {
+						result[i] = map[i];
+						done = false;
+						start++;
+					}
+				}
+				if (done) {
+					console.log(name, start);
+				}
+				next();
+			});
+		},
+		function(err) {
+			callback(err, result);
+		}
+	);
+}
+
+async.series([
+	function(next) {
+		Exporter.setup({
+			dbhost: 'localhost',
+			dbport: 5432,
+			dbname: 'discourse',
+			dbuser: 'postgres',
+			dbpass: '',
+
+			tablePrefix: 'restore.',
+		}, next);
+	},
+	function(next) {
+		getAll('Users', next);
+	},
+	function(next) {
+		getAll('Categories', next);
+	},
+	function(next) {
+		getAll('Topics', next);
+	},
+	function(next) {
+		getAll('Posts', next);
+	},
+	function(next) {
+		Exporter.teardown(next);
+	}
+], function(err, results) {
+	if (err) {
+		throw err;
+	}
+
+	fs.writeFileSync('./results.json', JSON.stringify(results, undefined, '\t'));
+});
