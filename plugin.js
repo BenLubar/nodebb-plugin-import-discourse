9a243b2e281f7c06642cafe205251460a28534fc
Ben Lubar <ben.lubar@gmail.com>
2015-11-23 18:16:19 -0600

    version 0.0.10: initial work on a redirect mechanism

---
+++ plugin.js
@@ -0,0 +1,16 @@
+(function(Plugin) {
+	var db = module.parent.require('./database');
+
+	Plugin.load = function(params) {
+		params.router.get('/t/:title/:tid(/:post_index)?', Plugin.topicRedirect);
+	};
+
+	Plugin.topicRedirect = function(req, res, next) {
+		db.sortedSetScore('_imported:_topics', req.params.tid, function(err, id) {
+			if (err || !id) {
+				return next();
+			}
+			res.redirect(301, nconf.get('url') + '/topic/' + id + '/' + req.params.title + (req.params.post_index ? '/' + req.params.post_index : ''));
+		});
+	};
+}(module.exports);
