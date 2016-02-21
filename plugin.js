49a9fba65d385c9cb12e5af6e2b11c46c9d0b996
Ben Lubar <ben.lubar@gmail.com>
2016-02-21 12:39:09 -0600

    add user_avatar redirect

--- plugin.js
+++ plugin.js
@@ -1,10 +1,12 @@
 (function(Plugin) {
 	var db = module.parent.require('./database'),
-	    Posts = module.parent.require('./posts');
+	    Posts = module.parent.require('./posts'),
+	    User = module.parent.require('./user');
 
 	Plugin.load = function(params, callback) {
 		params.router.get('/t/:title/:tid/:post_index?', Plugin.topicRedirect);
 		params.router.get('/p/:pid', Plugin.postRedirect);
+		params.router.get('/user_avatar/:host/:user/:size/:name', Plugin.avatarRedirect);
 
 		callback();
 	};
@@ -35,6 +37,23 @@
 
 					res.redirect(301, '/topic/' + tid + '/by-post/' + index);
 				});
+			});
+		});
+	};
+
+	Plugin.avatarRedirect = function(req, res, next) {
+		User.getUidByUserslug(req.params.user, function(err, id) {
+			if (err || !id) {
+				return next();
+			}
+
+			User.getUserField(id, 'picture', function(err, url) {
+				if (err || !url) {
+					return next();
+				}
+
+				res.redirect(302, url);
+			});
 		});
 	};
 })(module.exports);
