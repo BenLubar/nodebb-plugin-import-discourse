7c66823e5dc22c7c934829e867879fb2bbb36a9c
Ben Lubar <ben.lubar@gmail.com>
2016-02-21 14:31:45 -0600

    add category redirect for discourse

--- plugin.js
+++ plugin.js
@@ -1,12 +1,14 @@
 (function(Plugin) {
 	var db = module.parent.require('./database'),
 	    Posts = module.parent.require('./posts'),
-	    User = module.parent.require('./user');
+	    User = module.parent.require('./user'),
+	    Categories = module.parent.require('./categories');
 
 	Plugin.load = function(params, callback) {
 		params.router.get('/t/:title/:tid/:post_index?', Plugin.topicRedirect);
 		params.router.get('/p/:pid', Plugin.postRedirect);
 		params.router.get('/user_avatar/:host/:user/:size/:name', Plugin.avatarRedirect);
+		params.router.get('/c/:parent/:child?', Plugin.categoryRedirect);
 
 		callback();
 	};
@@ -54,6 +56,26 @@
 
 				res.redirect(302, url);
 			});
+		});
+	};
+
+	Plugin.categoryRedirect = function(req, res, next) {
+		var slug = req.params.child || req.params.parent;
+
+		Categories.getAllCategoryFields(['cid', 'slug'], function(err, cats) {
+			if (err) {
+				return next();
+			}
+
+			if (!cats.some(function(cat) {
+				if (cat.slug === cat.cid + '/' + slug) {
+					res.redirect(301, '/category/' + cat.slug);
+					return true;
+				}
+				return false;
+			})) {
+				next();
+			}
 		});
 	};
 })(module.exports);
